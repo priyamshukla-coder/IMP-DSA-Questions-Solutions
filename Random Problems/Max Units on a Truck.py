701f6a73c50ca53e945da8bdcfa92d83fcd53b66
Priyam-Shukla-EPAM <priyam_shukla@epam.com>
2022-08-01 16:43:43 +0530

    .\Random Problems\Max Units on a Truck.py

---
+++ Random Problems/Max Units on a Truck.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key = lambda x: -x[1])
+        print(boxTypes)
+        ans=0
+        for i in boxTypes:
+            a,b=i[0],i[1]
+            if truckSize>a:
+                truckSize=truckSize-a
+                ans=ans+(a*b)
+            else:
+                ans=ans+(truckSize*b)
+                return ans
+        return ans
