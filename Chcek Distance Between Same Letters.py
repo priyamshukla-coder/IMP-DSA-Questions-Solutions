290ef03b96ae2b33390e3c7564185fbb312ad716
priyamshukla-coder <priyamshukla66702@gmail.com>
2022-09-05 22:00:29 +0530

    Added Check distance between same letters

--- Chcek Distance Between Same Letters.py
+++ Chcek Distance Between Same Letters.py
@@ -2,25 +2,38 @@ class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
         
         def second_occur(s,curr_idx,char):
+            
             idx=curr_idx+1
+            
             while s[idx]!=char:
+                
                 idx=idx+1
                 
             return idx
             
         
         status=True
+        
         visited=[]
         
         for i in range(len(s)):
+            
             if s[i] not in visited:
+                
                 st_idx=i
+                
                 lst_idx=second_occur(s,i,s[i])
+                
                 # print(st_idx,lst_idx)
+                
                 # print(lst_idx-st_idx-1)
+                
                 if distance[ord(s[i])-97]==lst_idx-st_idx-1:
+                    
                     status=True
+                    
                 else:
+                    
                     return False
                 
                 visited.append(s[i])
