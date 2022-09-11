e82415be847840e00e89258dd4d2d7a95fe021d5
priyamshukla-coder <priyamshukla66702@gmail.com>
2022-09-11 13:17:30 +0530

    Added Optimal Partition of String

---
+++ Optimal Partition of String.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        
+        d1={}
+        cnt=0
+        for i in s:
+            
+            if i not in d1:
+                d1[i]=1
+            
+            else:
+                cnt=cnt+1
+                d1={}
+                d1[i]=1
+        
+        return cnt+1
+            
+            
