cad68e7faa2bcdc613f90140c2bf3728da58c712
priyamshukla-coder <priyamshukla66702@gmail.com>
2022-09-13 19:33:19 +0530

    Added Single Number

---
+++ Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        xor=nums[0]
+        
+        for i in nums[1:]:
+            
+            xor=xor^i
+            
+        return xor
