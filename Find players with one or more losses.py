7e6758ff3da8da0bcf693b1c1c5c1752d07a83d1
priyamshukla-coder <priyamshukla66702@gmail.com>
2022-11-29 01:07:15 +0530

    Added Find players with one or more loss

--- Find players with one or more losses.py
+++ Find players with one or more losses.py
@@ -1,27 +1,36 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        d1={i:0 for i in range(1,100000+1)}
-        d2={i:0 for i in range(1,100000+1)}
+        # d1={i:0 for i in range(1,100000+1)}
+        # d2={i:0 for i in range(1,100000+1)}
+
+        d1=defaultdict(lambda : [0,0])
+
         for i in matches:
-            a,b=i[0],i[1]
-            if a in d1:
-                d1[a]=d1[a]+1
-            else:
-                d1[a]=1
-            if b not in d2:
-                d2[b]=1
-            else:
-                d2[b]=d2[b]+1
-        
-        w,loss,res=[],[],[]
-        for i in range(1,100000+1):
-            if d1[i]>=1 and d2[i]==0:
-                w.append(i)
-            else:
-                if d2[i]==1:
-                    loss.append(i)
-        res.append(w)
-        res.append(loss)
+
+            win,loss=i[0],i[1]
+
+            d1[win][0]=d1[win][0]+1
+
+            d1[loss][1]=d1[loss][1]+1
+
+        win,loss,res=[],[],[[],[]]
+        for i in d1:
+
+            if d1[i][1]==0:
+
+                res[0].append(i)
+
+            elif d1[i][1]==1:
+
+                res[1].append(i)
+
+        res[0].sort()
+
+        res[1].sort()
+
+        # res.append(win)
+
+        # res.append(loss)
 
         return res
         
