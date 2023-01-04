1b22e9d3aafe094685d08c4640838e19c1882210
priyamshukla-coder <priyamshukla66702@gmail.com>
2023-01-04 22:07:41 +0530

    Added Minimum Rounds to complete all tasks

--- Minimum Rounds to complete all tasks.py
+++ Minimum Rounds to complete all tasks.py
@@ -5,16 +5,20 @@ Problem Link : https://leetcode.com/problems/minimum-rounds-to-complete-all-task
 
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        
-        c=Counter(tasks)
-        
-        for i in c:
-            if c[i]<2:
+
+        cnt=Counter(tasks)
+
+        for i in cnt:
+
+            if cnt[i]<=1:
+
                 return -1
-    
+
+
         min_time=0
-        
-        for i in c:
-            min_time=min_time+((c[i]//3) if c[i]%3==0 else c[i]//3+1)
-            #print(min_time)
+
+        for i in cnt:
+
+            min_time=min_time+ (cnt[i]//3 if cnt[i]%3==0 else cnt[i]//3+1)
+
         return min_time
